3e0dfc473377d7a63c220893bfec61b1218dd3f6
Saidazizxon <saidazizsaidnabiev209@gmail.com>
2024-11-10 23:11:07 +0500

    part of about component

---
+++ src/Component/About.jsx
@@ -0,0 +1,109 @@
+/* eslint-disable react/no-unescaped-entities */
+
+const About = () => {
+    return (
+        <>
+            <div>
+                <div className="mb-10 pt-[112px] px-[20px] pb-[240px] mx-auto">
+                    <div className="ml-20">
+                        <h5 className="uppercase text-[#1171e9] tracking-wide text-base font-medium">YOU'RE IN GREAT COMPANY</h5>
+                    </div>
+                    <div className="flex justify-around items-center">
+                        <div className="w-[600px]">
+                            <h2 className="font-semibold text-4xl">Trusted by over 2,400 schools, 120,000 classrooms,and 825,000 teachers and students.</h2>
+                        </div>
+                        <div className="grid grid-cols-3 gap-10 items-center">
+                            <div className="relative">
+                                <a href="">
+                                    <img
+                                        src="https://www.vivi.io/wp-content/uploads/2024/02/mvla_uhsd_logo_grey.png"
+                                        alt="grey_icon"
+                                        className="transition-opacity duration-300 ease-in-out"
+                                    />
+
+                                    <img
+                                        src="https://www.vivi.io/wp-content/uploads/2024/02/mvla_uhsd_logo.png"
+                                        alt="blue_icon"
+                                        className="absolute inset-0 opacity-0 hover:opacity-100 transition-opacity duration-300 ease-in-out"
+                                    />
+                                </a>
+                            </div>
+                            <div className="relative">
+                                <a href="">
+                                    <img 
+                                        src="https://www.vivi.io/wp-content/uploads/2024/02/St-Michaels-College-Logo_grey.png" 
+                                        alt="grey_icon"
+                                        className="transition-opacity duration-300 ease-in-out"
+                                    />
+                                    <img
+                                        src="https://www.vivi.io/wp-content/uploads/2024/02/St-Michaels-College-Logo.png" 
+                                        alt="blue_icon"
+                                        className="absolute inset-0 opacity-0 hover:opacity-100 transition-opacity duration-300 ease-in-out" 
+                                    />
+                                </a>
+                            </div>
+                            <div className="relative">
+                                <a href="">
+                                    <img 
+                                        src="https://www.vivi.io/wp-content/uploads/2024/02/Grand-Rapids-Public-Schools_grey.png" 
+                                        alt="grey_icon"
+                                        className="transition-opacity duration-300 ease-in-out"
+                                    />
+                                    <img
+                                        src="https://www.vivi.io/wp-content/uploads/2024/02/Grand-Rapids-Public-Schools_clr.png" 
+                                        alt="blue_icon"
+                                        className="absolute inset-0 opacity-0 hover:opacity-100 transition-opacity duration-300 ease-in-out" 
+                                    />
+                                </a>
+                            </div>
+                            <div className="relative">
+                                <a href="">
+                                    <img 
+                                        src="https://www.vivi.io/wp-content/uploads/2024/02/marryatville_grey.png" 
+                                        alt="grey_icon" 
+                                        className="transition-opacity duration-300 ease-in-out"
+                                    />
+                                    <img 
+                                        src="https://www.vivi.io/wp-content/uploads/2024/02/marryatville.png" 
+                                        alt="blue_icon" 
+                                        className="absolute inset-0 opacity-0 hover:opacity-100 transition-opacity duration-300 ease-in-out" 
+                                    />
+                                </a>
+                            </div>
+                            <div className="relative">
+                                <a href="">
+                                    <img 
+                                        src="https://www.vivi.io/wp-content/uploads/2024/02/logo_CGHS_grey.png" 
+                                        alt="grey_icon"
+                                        className="transition-opacity duration-300 ease-in-out"
+                                    />
+                                    <img 
+                                        src="https://www.vivi.io/wp-content/uploads/2024/02/logo_CGHS.png" 
+                                        alt="blue_icon" 
+                                        className="absolute inset-0 opacity-0 hover:opacity-100 transition-opacity duration-300 ease-in-out" 
+                                    />
+                                </a>
+                            </div>
+                            <div className="relative">
+                                <a href="">
+                                    <img 
+                                        src="https://www.vivi.io/wp-content/uploads/2024/02/Kensington_Park_School_grey.png" 
+                                        alt="grey_icon"
+                                        className="transition-opacity duration-300 ease-in-out" 
+                                    />
+                                    <img 
+                                        src="https://www.vivi.io/wp-content/uploads/2024/02/Kensington_Park_School_clr.png" 
+                                        alt="blue_icon" 
+                                        className="absolute inset-0 opacity-0 hover:opacity-100 transition-opacity duration-300 ease-in-out" 
+                                    />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default About
